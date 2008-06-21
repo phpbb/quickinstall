2f2575285c42e1ca23aa63806e97b9f675b3822a
Igor Wiedler <igor@wiedler.ch>
2008-06-21 09:14:40 +0000

    another attempt at adding a list of boards to the qi main page

--- modules/qi_main.php
+++ modules/qi_main.php
@@ -26,6 +26,22 @@ class qi_main
 		global $db, $template, $user;
 		global $quickinstall_path, $phpbb_root_path, $phpEx, $config, $qi_config;
 
+		// list of boards
+		$d = dir($qi_config['boards_dir']);
+		while (false !== ($file = $d->read()))
+		{
+			if (in_array($file, array('.', '..', '.svn', '.htaccess'), true) || is_file($qi_config['boards_dir'] . $file))
+			{
+				continue;
+			}
+
+			$template->assign_block_vars('row', array(
+				'BOARD_NAME'	=> htmlspecialchars($file),
+				'BOARD_URL'		=> $qi_config['boards_dir'] . urlencode($file),
+			));
+		}
+		$d->close();
+
 		// Assign index specific vars
 		$template->assign_vars(array(
 			'U_CREATE'		=> qi::url('create'),
@@ -33,6 +49,8 @@ class qi_main
 			'TABLE_PREFIX'	=> htmlspecialchars($qi_config['table_prefix']),
 			'SITE_NAME'		=> $qi_config['site_name'],
 			'SITE_DESC'		=> $qi_config['site_desc'],
+
+			'PAGE_MAIN'		=> true,
 		));
 
 		// Output page
