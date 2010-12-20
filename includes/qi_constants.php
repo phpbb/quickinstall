bb9f2300f15063a55419669f72f84c9a13523028
tumba25 <tumba25@gmail.com>
2010-12-20 05:52:31 +0200

    Added last changes to the changelog.

--- includes/qi_constants.php
+++ includes/qi_constants.php
@@ -8,7 +8,7 @@
 *
 */
 
-define('QI_VERSION', '1.1.6-dev');
+define('QI_VERSION', '1.1.6-RC2');
 
 define('QI_PHPBB_VERSION', '3.0.8'); // Version for the latest phpBB
-define('AUTOMOD_VERSION', '1.0.0'); // Version for the included AutoMOD
+define('AUTOMOD_VERSION', '1.0.0'); // Version for the latest AutoMOD
