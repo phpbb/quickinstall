daebe5d7ccbf5471e4c3fa2b8269e6d534b6c4bb
tumba25 <tumba25@phpbb.com>
2010-12-22 18:35:24 +0200

    Added link to wikipedia

--- includes/lorem_ipsum.php
+++ includes/lorem_ipsum.php
@@ -12,6 +12,7 @@
 
 /**
  * This file contains only lorem ipsum, a dummy text to fill the posts.
+ * http://en.wikipedia.org/wiki/Lorem_ipsum
  * This string should not be translated but you can edit it if you want to.
  */
 
@@ -24,4 +25,3 @@ if (!defined('IN_QUICKINSTALL'))
 }
 
 $lorem_ipsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-
