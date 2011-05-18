3b2b69dd909c4b3be64c43873caa54d996d3b8ee
Oleg Pudeyev <oleg@bsdpower.com>
2011-05-18 05:40:11 -0400

    Connect to postgres database on postgresql.
    PostgreSQL requires a database to connect to, unlike some other database
    engines. postgres is a default database for just such occasions.
    
    Add an override for sql_connect to connect to postgres database if
    no database was specified.

--- includes/db/postgres.php
+++ includes/db/postgres.php
@@ -20,4 +20,15 @@ if (!defined('IN_QUICKINSTALL'))
 */
 class dbal_postgres_qi extends dbal_postgres
 {
+	public function sql_connect($dbhost, $dbuser, $dbpass, $dbname, $dbport = false, $persistency = false, $new_link=  false)
+	{
+		if ($dbname === false)
+		{
+			// PostgreSQL requires a database to connect to.
+			// postgres is the default database as described in the manual:
+			// http://www.postgresql.org/docs/9.0/static/manage-ag-templatedbs.html
+			$dbname = 'postgres';
+		}
+		return parent::sql_connect($dbhost, $dbuser, $dbpass, $dbname, $dbport, $persistency, $new_link);
+	}
 }
