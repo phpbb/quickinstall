851749712766dbc8153791e7046dc5b32250f505
Oleg Pudeyev <oleg@bsdpower.com>
2011-05-18 00:20:34 -0400

    Missed postgres db class

---
+++ includes/db/postgres.php
@@ -0,0 +1,23 @@
+<?php
+/**
+*
+* @package quickinstall
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_QUICKINSTALL'))
+{
+	exit;
+}
+
+/**
+* Postgres dbal extension
+* @package dbal
+*/
+class dbal_postgres_qi extends dbal_postgres
+{
+}
