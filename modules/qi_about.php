c71a8fb2b7f524e2f261ef4c5e499a6d6c16f9ca
Jari Kanerva <tumba25@phpbb.com>
2012-12-10 17:22:12 +0200

    Removed version check from the About page and moved the changelog stuff a bit.

--- modules/qi_about.php
+++ modules/qi_about.php
@@ -26,31 +26,8 @@ class qi_about
 		global $db, $template, $user, $settings;
 		global $quickinstall_path, $phpbb_root_path, $phpEx, $config, $qi_config;
 
-		if ($qi_config['version_check'])
-		{
-			// we use this for get_remote_file()
-			include($phpbb_root_path . 'includes/functions_admin.' . $phpEx);
-
-			// Get current and latest version
-			$errstr = '';
-			$errno = 0;
-
-			$info = get_remote_file('phpbbmodders.net', '/files/updatecheck', 'quickinstall.txt', $errstr, $errno);
-
-			if ($info !== false)
-			{
-				list($latest_version, $announcement_url) = explode("\n", $info);
-				$up_to_date = (version_compare(str_replace('rc', 'RC', strtolower(QI_VERSION)), str_replace('rc', 'RC', strtolower($latest_version)), '<')) ? false : true;
-
-				$template->assign_vars(array(
-					'UP_TO_DATE'	=> $up_to_date,
-					'L_UPDATE'		=> sprintf($user->lang['UPDATE_TO'], $announcement_url, $latest_version),
-				));
-			}
-		}
-
 		$changelog_file = $quickinstall_path . 'CHANGELOG';
-		if ($use_changelog = file_exists($changelog_file))
+		if (file_exists($changelog_file))
 		{
 			// let's get the changelog :)
 			$data = file($changelog_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
@@ -58,8 +35,6 @@ class qi_about
 			// We do not want the first line.
 			unset($data[0]);
 
-			$changes_ary = array();
-			$key = 0; // Make sure the key is set to something.
 			foreach ($data as $row)
 			{
 				$row = ltrim($row);
@@ -67,22 +42,15 @@ class qi_about
 				if ($row[0] == '-')
 				{
 					$key = substr($row, 2);
-					$changes_ary[$key] = array();
+
+					$template->assign_block_vars('history', array(
+						'CHANGES_SINCE'	=> $key,
+					));
 				}
 				else
 				{
-					$changes_ary[$key][] = substr($row, 2);
-				}
-			}
+					$change = substr($row, 2);
 
-			foreach ($changes_ary as $key => $entry)
-			{
-				$template->assign_block_vars('history', array(
-					'CHANGES_SINCE'	=> $key,
-				));
-
-				foreach ($entry as $change)
-				{
 					$template->assign_block_vars('history.changelog', array(
 						'CHANGE'	=> htmlspecialchars($change),
 					));
@@ -93,8 +61,6 @@ class qi_about
 		$template->assign_vars(array(
 			'S_IN_INSTALL' => false,
 			'S_IN_SETTINGS' => false,
-			'S_ALLOW_VERSION_CHECK'	=> $qi_config['version_check'],
-			'S_ALLOW_CHANGELOG'		=> $use_changelog,
 			'PAGE_MAIN'		=> false,
 		));
 
