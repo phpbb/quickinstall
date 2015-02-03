2f8f8becd7bd2fa808c294fe1999d088d837a5bd
tumba25 <tumba25@phpbb.com>
2015-02-03 19:17:56 +0100

    Bump version to 1.2.1-RC1

--- includes/qi_constants.php
+++ includes/qi_constants.php
@@ -7,7 +7,7 @@
 *
 */
 
-define('QI_VERSION', '1.2.1-dev');
+define('QI_VERSION', '1.2.1-RC1');
 
 // Cookies set by QI
 define('QI_PROFILE_COOKIE', 'qi_profile');	// Cookie with the latest used profile name as payload.
