3b6203dd4a787798b99adb7421b5e6660e649c0d
tumba25 <tumba25@phpbb.com>
2015-02-14 22:52:44 +0100

    Bump the version number to 1.2.1

--- includes/qi_constants.php
+++ includes/qi_constants.php
@@ -7,7 +7,7 @@
 *
 */
 
-define('QI_VERSION', '1.2.1-RC2');
+define('QI_VERSION', '1.2.1');
 
 // Cookies set by QI
 define('QI_PROFILE_COOKIE', 'qi_profile');	// Cookie with the latest used profile name as payload.
