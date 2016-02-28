35b1a9febc48be9dd3c8dbc8a9ed5d09f3c44cbd
Matt Friedman <maf675@gmail.com>
2016-02-28 14:33:12 -0800

    Also don't show AutoMOD options to 3.1 3.2 environments

--- modules/qi_main.php
+++ modules/qi_main.php
@@ -71,7 +71,7 @@ class qi_main
 
 			'ALT_ENV'		=> get_alternative_env($settings->get_config('alt_env')),
 
-			'PHPBB_VERSION'	=> defined('PHPBB_32') ? 'PHPBB_32' : (defined('PHPBB_31') ? 'PHPBB_31' : 'PHPBB_30'),
+			'PHPBB_VERSION'	=> defined('PHPBB_32') ? '3.2' : (defined('PHPBB_31') ? '3.1' : '3.0'),
 
 			// Chunk settings
 			'CHUNK_POST'	=> $settings->get_config('chunk_post', 0),
