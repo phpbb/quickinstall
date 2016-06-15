1f7e8de3c9919db6eb547ccf43cfe0a680d4f918
Matt Friedman <maf675@gmail.com>
2016-06-15 15:29:01 -0700

    Bump version to 1.2.3

--- includes/qi_constants.php
+++ includes/qi_constants.php
@@ -7,7 +7,7 @@
 *
 */
 
-define('QI_VERSION', '1.2.2');
+define('QI_VERSION', '1.2.3');
 
 // Cookies set by QI
 define('QI_PROFILE_COOKIE', 'qi_profile');	// Cookie with the latest used profile name as payload.
