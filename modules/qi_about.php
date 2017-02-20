3a0609cf8e0a87ad614fc15af16f16218a600f8a
Matt Friedman <maf675@gmail.com>
2017-02-20 08:24:26 -0800

    Update changelog and documentation to read markdown

--- modules/qi_about.php
+++ modules/qi_about.php
@@ -22,23 +22,26 @@ class qi_about
 {
 	public function __construct()
 	{
-		global $db, $template, $user, $settings, $s_docs;
-		global $quickinstall_path, $phpbb_root_path, $phpEx, $config;
+		global $template, $user, $quickinstall_path, $phpEx;
 
-		if (legacy_request_var('page', '') == 'docs')
+		include $quickinstall_path . 'includes/ParseDown.' . $phpEx;
+		$Parsedown = new Parsedown();
+
+		$s_docs = legacy_request_var('page', '') === 'docs';
+
+		if ($s_docs)
 		{
-			$s_about	= false;
-			$s_docs		= true;
+			$doc_file = $quickinstall_path . 'README.md';
+
+			if (file_exists($doc_file))
+			{
+				$doc_body = file_get_contents($doc_file);
+				$template->assign_var('DOC_BODY', $Parsedown->text($doc_body));
+			}
 		}
 		else
 		{
-			$s_about	= true;
-			$s_docs		= false;
-		}
-
-		if ($s_about)
-		{
-			$changelog_file = $quickinstall_path . 'CHANGELOG';
+			$changelog_file = $quickinstall_path . 'CHANGELOG.md';
 
 			if (file_exists($changelog_file))
 			{
@@ -52,9 +55,9 @@ class qi_about
 				{
 					$row = ltrim($row);
 
-					if ($row[0] == '-')
+					if ($row[0] === '#')
 					{
-						$key = substr($row, 2);
+						$key = substr($row, 3);
 
 						$template->assign_block_vars('history', array(
 							'CHANGES_SINCE'	=> $key,
@@ -62,115 +65,25 @@ class qi_about
 							'U_CHANGES'	=> strtolower(str_replace(' ', '-', $key)),
 						));
 					}
-					else
+					else if ($row[0] === '-')
 					{
 						$change = substr($row, 2);
 
 						$template->assign_block_vars('history.changelog', array(
-							'CHANGE'	=> htmlspecialchars($change),
+							'CHANGE'	=> $Parsedown->line($change),
 						));
 					}
 				}
 			}
 		}
-		else // if ($s_docs)
-		{
-			$doc_file = $quickinstall_path . 'readme.txt';
-
-			if (file_exists($doc_file))
-			{
-				$file_ary	= file($doc_file, FILE_IGNORE_NEW_LINES);
-				$preg_url	= '!(http|http)(s)?:\/\/[a-zA-Z0-9.?&_/]+!';
-				$end		= sizeof($file_ary);
-
-				// We do not want the first line.
-				for ($i = 1; $i < $end; $i++)
-				{
-					if (empty($file_ary[$i]) && empty($file_ary[$i + 1]))
-					{
-						$i = $i + 2;
-						$doc_row = "</p>\n<h3>{$file_ary[$i]}</h3>\n<p>";
-					}
-					else if (empty($file_ary[$i]) && !empty($file_ary[$i + 1]))
-					{
-						$doc_row = "<br /><br />\n";
-					}
-					else
-					{
-						if(preg_match($preg_url, $file_ary[$i], $url))
-						{
-							$doc_row = preg_replace($preg_url, "<a href=\"\\0\">\\0</a>",$file_ary[$i]);
-						}
-						else
-						{
-							$doc_row = "$file_ary[$i]\n";
-						}
-					}
-
-					$template->assign_block_vars('doc_row', array(
-						'ROW' => (!empty($doc_row)) ? $doc_row : '',
-					));
-				}
-
-				$template->assign_block_vars('doc_row', array(
-					'ROW' => '</p>',
-				));
-/*
-				foreach ($data as $row)
-				{
-					if (empty($row))
-					{
-						if ($p_start && $empty_row == 1)
-						{
-							$doc_row = "</p>\n";
-							$p_start = false;
-						}
-						else if ($p_start && $empty_row == 0)
-						{
-							$doc_row = "<br />\n";
-						}
-
-						$empty_row++;
-					}
-					else if ($empty_row == 2)
-					{
-						$doc_row = str_replace(':', '', $row);
-						$doc_row = "<h3>$doc_row</h3>\n<p>";
-						$p_start = true;
-						$empty_row = 0;
-					}
-					else
-					{
-						if(preg_match($preg_url, $row, $url))
-						{
-							$doc_row = preg_replace($preg_url, "<a href=\"\\0\">\\0</a>",$row);
-						}
-						else
-						{
-							$doc_row = "$row\n";
-						}
-
-						$empty_row = 0;
-					}
-
-					$template->assign_block_vars('doc_row', array(
-						'ROW' => (!empty($doc_row)) ? $doc_row : '',
-					));
-
-					$doc_row = '';
-				}
-*/
-
-			}
-		}
 
 		$template->assign_vars(array(
-			'S_ABOUT'	=> $s_about,
 			'S_DOCS'	=> $s_docs,
+			'S_ABOUT'	=> !$s_docs,
 		));
 
 		// Output page
-		qi::page_header((($s_about) ? $user->lang['QI_ABOUT'] : $user->lang['QI_ABOUT']), ' ');
+		qi::page_header(($s_docs ? $user->lang['DOCS_LONG'] : $user->lang['QI_ABOUT']), ' ');
 
 		$template->set_filenames(array(
 			'body' => 'about_body.html',
