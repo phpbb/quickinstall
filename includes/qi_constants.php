5c25e9b40d2ea753d49d1e6aac5eada72fed7c37
Matt Friedman <maf675@gmail.com>
2017-05-20 21:22:32 -0700

    Set version to 1.3.0-RC1

--- includes/qi_constants.php
+++ includes/qi_constants.php
@@ -7,7 +7,7 @@
 *
 */
 
-define('QI_VERSION', '1.3.0-dev');
+define('QI_VERSION', '1.3.0-RC1');
 
 // Cookies set by QI
 define('QI_PROFILE_COOKIE', 'qi_profile');	// Cookie with the latest used profile name as payload.
