89d17a81a65d0408ab6bb53e028e669932f9e38a
Matt Friedman <maf675@gmail.com>
2017-07-08 23:37:15 -0700

    Use array_unshift to get top version

--- includes/qi_version_helper.php
+++ includes/qi_version_helper.php
@@ -154,7 +154,7 @@ class qi_version_helper
 		try
 		{
 			$updates = $this->get_suggested_updates();
-			return reset($updates);
+			return array_shift($updates);
 		}
 		catch (RuntimeException $e)
 		{
