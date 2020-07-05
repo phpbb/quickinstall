5320b25f7f289dd45ba470b97368c739a693e50d
Matt Friedman <maf675@gmail.com>
2020-07-05 10:32:08 -0700

    Add QI_DEBUG constant

--- includes/qi_constants.php
+++ includes/qi_constants.php
@@ -8,6 +8,7 @@
 */
 
 define('QI_VERSION', '1.4.0-dev');
+//define('QI_DEBUG', true); // un-comment this disable template caching
 
 // Cookies set by QI
 define('QI_PROFILE_COOKIE', 'qi_profile');	// Cookie with the latest used profile name as payload.
