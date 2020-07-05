d39a2cd7810b30183c31099d23a3e8ff628de159
Matt Friedman <maf675@gmail.com>
2020-07-04 17:43:24 -0700

    Version bump to 1.4.0-dev

--- includes/qi_constants.php
+++ includes/qi_constants.php
@@ -7,7 +7,7 @@
 *
 */
 
-define('QI_VERSION', '1.3.7');
+define('QI_VERSION', '1.4.0-dev');
 
 // Cookies set by QI
 define('QI_PROFILE_COOKIE', 'qi_profile');	// Cookie with the latest used profile name as payload.
