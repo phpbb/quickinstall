5fa8af25e7ec51bc0186ffdc2d0e83e82addf7b7
Matt Friedman <maf675@gmail.com>
2020-07-06 21:47:27 -0700

    lorem ipsum should be a lang var

--- language/en/qi.php
+++ language/en/qi.php
@@ -162,8 +162,9 @@ $lang = array_merge($lang, array(
 	'REQUIRED'				=> 'Required',
 
 	'LOG_INSTALL_INSTALLED_QI'	=> '<strong>Installed by phpBB Quickinstall version %s</strong>',
+	'LOREM_IPSUM'			=> 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 
-	'MAKE_WRITABLE'			=> 'Make files world writable',
+'MAKE_WRITABLE'			=> 'Make files world writable',
 	'MAKE_WRITABLE_EXPLAIN'	=> 'Set files, &quot;config.php&quot;, and directories world writable by default.',
 	'MAKE_WRITABLE_BOARD'	=> 'Make files world writable',
 	'MAKE_WRITABLE_BOARD_EXPLAIN'	=> 'Sets file permissions to 0666.',
