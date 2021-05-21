ab62854bee555c453329ad5faaee2cdcc70df0ba
Matt Friedman <maf675@gmail.com>
2021-05-20 22:23:45 -0700

    Overall tweak icon alignments

--- language/en/qi.php
+++ language/en/qi.php
@@ -251,7 +251,7 @@ $lang = array_merge($lang, array(
 	'QI_TZ'				=> 'Time zone',
 	'QI_TZ_EXPLAIN'		=> 'Set the default time zone for your boards.',
 	'QUICKINSTALL'		=> 'QuickInstall',
-	'QUICKINSTALL_LOGO'	=> 'Quick<svg class="bi text-white" width="16" height="16" fill="currentColor"><use xlink:href="%1$s/assets/img/bootstrap-icons.svg#lightning-fill"/></svg>Install',
+	'QUICKINSTALL_LOGO'	=> 'Quick%sInstall',
 
 	'REDIRECT'			=> 'Redirect',
 	'REDIRECT_EXPLAIN'	=> 'Redirect to new board after it is created.',
